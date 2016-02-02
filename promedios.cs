396d667ecb0a350f1c80538fba4a6ecd17cb545a
Luis <luishendrix92@gmail.com>
2016-02-02 00:09:03 -0800

    Started

--- promedios.cs
+++ promedios.cs
@@ -8,13 +8,13 @@ namespace ControlAlumnos {
       Console.WriteLine("Dame tu nombre.");
       string nombre = Console.ReadLine();
 
-      Console.WriteLine("Cuàl fue tu primer calificaciòn?");
+      Console.WriteLine("Cuál fue tu primer calificación?");
       float cal1 = float.Parse(Console.ReadLine());
 
-      Console.WriteLine("Cuàl fue tu segunda calificaciòn?");
+      Console.WriteLine("Cuál fue tu segunda calificación?");
       float cal2 = float.Parse(Console.ReadLine());
 
-      Console.WriteLine("Cuàl fue tu tercer calificaciòn?");
+      Console.WriteLine("Cuál fue tu tercer calificación?");
       float cal3 = float.Parse(Console.ReadLine());
 
       float promedio = (cal1 + cal2 + cal3) / 3;
@@ -23,5 +23,3 @@ namespace ControlAlumnos {
     } // Fin de mètodo Main
   } // Fin de clase principal
 } // Fin de nombre de espacio
-
-// Nom sem calif1 calif2 calif3 numctrl
---
+++ tarea1.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TareaUno {
+  class Programa {
+    public static void Main() {
+      Console.WriteLine("Hola Mundo");
+    } // Fin de Método Main
+  } // Fin de clase
+} // Fin de namespace
