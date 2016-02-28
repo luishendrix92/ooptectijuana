b8039b6122a4d4155fb77cb80bf0cb373d4ff0cd
Luis <luishendrix92@gmail.com>
2016-02-28 15:21:01 -0800

    tarea 6 pre

---
+++ Unidad2/Cuenta Bancaria/cuenta.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Tarea6 {
+  class CuentaBancaria {
+    private int    numero ;
+    private float  saldo  ;
+    private string titular;
+    static  int    cuentas;
+
+    public int NumeroCuenta {
+      get { return numero;  }
+      set { numero = value; }
+    } public float Saldo {
+      // No estoy seguro si sea
+      // buena idea dar acceso
+      // a esta propiedad...
+      get { return saldo;  }
+      set { saldo = value; }
+    } public string Titular {
+      get { return titular;  }
+      set { titular = value; }
+    } // Fin de getters y setters
+
+    public CuentaBancaria() {
+      cuentas += 1;
+      numero   = cuentas;
+    } public CuentaBancaria(float s, string t) {
+      cuentas += 1;
+      numero   = cuentas;
+      saldo    = s;
+      titular  = t;
+    } // Fin de constructor sobrecargado
+
+    public void Deposito(float monto) {
+      if (monto > 0) {
+        saldo += monto;
+        Console.WriteLine("Depósito realizado.");
+        Console.WriteLine("> Balance: {0:C2}", saldo);
+      } else { // Monto fue de 0 o negativo
+        Console.WriteLine("El monto debe ser positivo.");
+      } // Fin de comprobar monto positivo
+    } // Fin de depositar saldo a cuenta
+
+    public float Retiro(float monto) {
+      if (saldo >= monto && monto > 0) {
+        Console.WriteLine("Retiro realizado.");
+        Console.WriteLine("> Balance: {0:C2}", saldo);
+        saldo -= monto;
+        return monto;
+      } else { // No hay dinero suficiente
+        Console.WriteLine("Retiro fallido.");
+        return 0;
+      } // Fin de ver si hay dinero
+    } // Fin de retirar saldo de la cuenta
+  } // Fin de clase CuentaBancaria
+} // Fin de espacio de nombre
---
+++ Unidad2/Cuenta Bancaria/main.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Tarea6 {
+  class Programa {
+    public static void Main() {
+      Console.Clear();
+      Cuenta falsa = new CuentaBancaria();
+      Cuenta buena = new CuentaBancaria();
+
+      // Cambiar atributos de cuentas
+      falsa.Titular = "Menganito Perez";
+      falsa.Saldo   = 44.65f;
+      buena.Titular = "Luis Felipe López";
+
+      // Manejo de fondos de cuenta
+      buena.Deposito(50000);
+
+      // Retirar de buena y abonar a falsa
+      falsa.Deposito(buena.Retiro(24500.5f));
+
+      // Tratar de realizar malos movimientos
+      falsa.Retiro(40000)  ;
+      buena.Deposito(-4.5f);
+    } // Fin de Método Main
+  } // Fin de clase principal
+} // Fin de espacio de nombre
